e76d0630d5de614bb65b930c800d2736bbc36f03
ganti <28899559+ganti@users.noreply.github.com>
2022-08-08 19:38:00 +0200

    Update timezone

--- app/src/Kernel.php
+++ app/src/Kernel.php
@@ -12,6 +12,6 @@ class Kernel extends BaseKernel
     public function boot()
     {
         parent::boot();
-        date_default_timezone_set($this->getContainer()->getParameter('timezone'));
+        date_default_timezone_set($this->getContainer()->getParameter('app')['timezone']);
     }
 }
